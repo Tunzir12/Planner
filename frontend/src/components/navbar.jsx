28a004fb205d2897810aa88e2299580060769f12
Maliha Tunzira <mtunjira@yahoo.com>
2025-04-09 10:38:59 +0200

    creating pages

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='box'>
+        <div className="form">
+            <label htmlFor="">Name:</label>
+            <input type="text" placeholder='Full Name' />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
