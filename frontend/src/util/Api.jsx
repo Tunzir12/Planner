f7fd0e875148bceac2340c2e7b8f8ff77ea42793
Robin Boregrim <robin.boregrim@gmail.com>
2025-04-22 11:42:23 +0200

    Dummy api restructure

--- frontend/src/util/Api.jsx
+++ frontend/src/util/Api.jsx
@@ -12,44 +12,6 @@ const projects = [
 
     deadline: "Never",
 
-    goals: [
-
-      {
-        title: "Do thing",
-        assigned: "Me",
-        status: 2,
-        deadline: "Yesterday",
-      },
-
-      {
-        title: "Do other thing",
-        assigned: "Me",
-        status: 1,
-        deadline: "Today",
-      },
-
-      {
-        title:
-          "Do nothing",
-        assigned:
-          "Friend",
-        status:
-          0,
-        deadline:
-          "Today",
-      },
-
-      {
-        title:
-          "Chill",
-        assigned:
-          "Me ",
-        status:
-          3,
-        deadline:
-          "Tomorrow",
-      },
-    ]
   },
   {
     name: "Hello",
@@ -62,24 +24,86 @@ const projects = [
     },
 
     deadline: "Never",
-    goals: [
-      {
-        title:
-          "Chill",
-        assigned:
-          "Me ",
-        status:
-          0,
-        deadline:
-          "Tomorrow",
-      },
-    ]
   },
 ];
 
-const getProjects = () => { return projects }
-const getProject = (id) => { return projects[id] }
+const projectGoals = [
+  { project: 0, goal: 0 },
+  { project: 0, goal: 1 },
+  { project: 0, goal: 2 },
+  { project: 0, goal: 3 },
+  { project: 1, goal: 4 },
+]
 
-export { getProjects, getProject }
+
+const goals =
+  [
+    {
+      title: "Do thing",
+      assigned: "Me",
+      status: 2,
+      deadline: "Yesterday",
+    },
+
+    {
+      title: "Do other thing",
+      assigned: "Me",
+      status: 1,
+      deadline: "Today",
+    },
+
+    {
+      title:
+        "Do nothing",
+      assigned:
+        "Friend",
+      status:
+        0,
+      deadline:
+        "Today",
+    },
+
+    {
+      title:
+        "Chill",
+      assigned:
+        "Me ",
+      status:
+        3,
+      deadline:
+        "Tomorrow",
+    },
+    {
+      title:
+        "Chill",
+      assigned:
+        "Me ",
+      status:
+        0,
+      deadline:
+        "Tomorrow",
+    },
+  ]
+
+const getProjects = () => { return projects }
+const getProject = (id) => {
+
+
+  var project = projects[id]
+
+  const goalIds = projectGoals.filter((relation) => (
+    relation.project == id
+  ));
+  project.goals = goalIds.map((relation) => (goals[relation.goal]))
+
+  return project
+
+}
+
+const getGoal = (id) => {
+  return goals[id]
+}
+
+export { getProjects, getProject, getGoal }
 
 
