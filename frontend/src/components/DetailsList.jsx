ced9e9daa9aa99406647f5554ce2df45783f130c
Robin Boregrim <robin.boregrim@gmail.com>
2025-05-09 13:42:45 +0200

    Added key prop to DetailsList

--- frontend/src/components/DetailsList.jsx
+++ frontend/src/components/DetailsList.jsx
@@ -4,7 +4,7 @@ const DetailsList = ({ headers, data }) => (
   <div className="flow-root my-6">
     <dl className="-my-3 divide-y divide-gray-200 text-sm dark:divide-gray-700">
       {headers.map((header, index) => (
-        <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+        <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4" key={index}>
           <dt className="font-medium text-gray-900 dark:text-white">{header}</dt>
           <dd className="text-gray-700 sm:col-span-2 dark:text-gray-200">{data[index]}</dd>
         </div>
