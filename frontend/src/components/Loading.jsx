2cf626b8b650289ed32bdc2e70ba2092fbc8086e
Robin Boregrim <robin.boregrim@gmail.com>
2025-05-16 18:45:20 +0200

    New Component Loadingscreen

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = ({ }) => (
+  <div className="dark:bg-gray-900 min-h-screen h-full">
+    <div className='text-white'>
+      Loading...
+    </div>
+  </div>
+)
+
+
+export default Loading;
