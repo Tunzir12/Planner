9ba7435db6e05dfec2072ac211817bc22643a5d1
Robin Boregrim <robin.boregrim@gmail.com>
2025-06-04 19:54:02 +0200

    Added optional keys to Table
    Keys can be used to keep track of elements when lazy updatig the list

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Table = ({ headers, data }) => {
+const Table = ({ headers, data, keys = [] }) => {
 
   return (
     <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400 ">
@@ -17,7 +17,7 @@ const Table = ({ headers, data }) => {
       <tbody>
         {data.map(
           (list, index) => (
-            <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <tr key={keys ? keys[index] : index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
               {list.map((element, elementIndex) => (
                 <td key={elementIndex} className="px-6 py-4">
                   {element}
