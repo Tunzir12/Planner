4f2359f969d3c1cd7516cd823c889ba214f18ea5
Tunzir12 <mtunzira1@gmail.com>
2025-11-13 00:57:37 +0100

    environment variables

--- backend/firebase.jsx
+++ backend/firebase.jsx
@@ -11,6 +11,7 @@ import {
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER,
